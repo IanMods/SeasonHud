e38660d31028b7e46361395650482736608a0ee0
Ian Anderson <lanAnderson@users.noreply.github.com>
2025-07-25 08:57:07 -0700

    1.21.1 update

--- forge/src/main/java/club/iananderson/seasonhud/forge/SeasonHudForge.java
+++ forge/src/main/java/club/iananderson/seasonhud/forge/SeasonHudForge.java
@@ -4,7 +4,6 @@ import club.iananderson.seasonhud.Common;
 import club.iananderson.seasonhud.config.Config;
 import fuzs.forgeconfigapiport.forge.api.neoforge.v4.NeoForgeConfigRegistry;
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.config.ModConfig;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
