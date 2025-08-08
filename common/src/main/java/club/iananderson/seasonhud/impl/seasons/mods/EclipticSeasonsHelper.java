74a9b8d8b3f3cf19c1fef1d33250d708e42fc20c
Ian Anderson <lanAnderson@users.noreply.github.com>
2025-08-07 22:30:44 -0700

    1.19 changes

--- common/src/main/java/club/iananderson/seasonhud/impl/seasons/mods/EclipticSeasonsHelper.java
+++ common/src/main/java/club/iananderson/seasonhud/impl/seasons/mods/EclipticSeasonsHelper.java
@@ -8,7 +8,7 @@ import com.teamtea.eclipticseasons.config.CommonConfig;
 import java.util.List;
 import java.util.Objects;
 import net.minecraft.client.Minecraft;
-import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.core.Registry;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.player.Player;
@@ -19,7 +19,7 @@ public class EclipticSeasonsHelper implements IModHelper {
   @Override
   public Item CALENDAR() {
     if (Common.eclipticSeasonsLoaded()) {
-      return BuiltInRegistries.ITEM.get(new ResourceLocation("seasons", "season_calendar"));
+      return Registry.ITEM.get(new ResourceLocation("seasons", "season_calendar"));
     }
     else {
       return null;
