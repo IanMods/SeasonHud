f293f8d468dee88ce4dfe1490daccb31503cc934
Ian Anderson <lanAnderson@users.noreply.github.com>
2025-08-21 15:10:17 -0700

    Bad workaround so I dont need to make a seperate build for 1.21.2-1.21.4 and 1.21.5

--- fabric/src/main/java/club/iananderson/seasonhud/fabric/client/SeasonHudClientFabric.java
+++ fabric/src/main/java/club/iananderson/seasonhud/fabric/client/SeasonHudClientFabric.java
@@ -5,8 +5,10 @@ import club.iananderson.seasonhud.config.Config;
 import club.iananderson.seasonhud.fabric.event.ClientEvents;
 import club.iananderson.seasonhud.impl.minimaps.CurrentMinimap;
 import club.iananderson.seasonhud.impl.minimaps.SeasonComponent;
+import club.iananderson.seasonhud.platform.Services;
 import dev.architectury.utils.Env;
 import dev.architectury.utils.EnvExecutor;
+import fuzs.forgeconfigapiport.fabric.api.neoforge.v4.NeoForgeConfigRegistry;
 import fuzs.forgeconfigapiport.fabric.api.v5.ConfigRegistry;
 import net.fabricmc.api.ClientModInitializer;
 import net.neoforged.fml.config.ModConfig;
@@ -15,8 +17,16 @@ public class SeasonHudClientFabric implements ClientModInitializer {
 
   @Override
   public void onInitializeClient() {
-    ConfigRegistry.INSTANCE.register(Common.MOD_ID, ModConfig.Type.CLIENT, Config.GENERAL_SPEC,
-                                     "seasonhud-client.toml");
+    if(Services.PLATFORM.getModVersion("forgeconfigapiport").startsWith("21.5")) {
+      ConfigRegistry.INSTANCE.register(Common.MOD_ID, ModConfig.Type.CLIENT, Config.GENERAL_SPEC,
+                                       "seasonhud-client.toml");
+    }
+
+    else{
+      NeoForgeConfigRegistry.INSTANCE.register(Common.MOD_ID, ModConfig.Type.CLIENT, Config.GENERAL_SPEC,
+                                               "seasonhud-client.toml");
+    }
+
     ClientEvents.register();
 
     if (CurrentMinimap.ftbChunksLoaded()) {
--- forge/src/main/java/club/iananderson/seasonhud/forge/SeasonHudForge.java
+++ forge/src/main/java/club/iananderson/seasonhud/forge/SeasonHudForge.java
@@ -2,7 +2,7 @@ package club.iananderson.seasonhud.forge;
 
 import club.iananderson.seasonhud.Common;
 import club.iananderson.seasonhud.config.Config;
-import fuzs.forgeconfigapiport.forge.api.v5.NeoForgeConfigRegistry;
+import club.iananderson.seasonhud.platform.Services;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.config.ModConfig;
@@ -16,8 +16,20 @@ public class SeasonHudForge {
     MinecraftForge.EVENT_BUS.register(this);
     Common.init();
 
-    NeoForgeConfigRegistry.INSTANCE.register(Common.MOD_ID, ModConfig.Type.CLIENT, Config.GENERAL_SPEC,
-                                             "seasonhud-client.toml");
+    if (Services.PLATFORM.getModVersion("forgeconfigapiport").startsWith("21.5")) {
+      fuzs.forgeconfigapiport.forge.api.v5.NeoForgeConfigRegistry.INSTANCE.register(Common.MOD_ID,
+                                                                                    ModConfig.Type.CLIENT,
+                                                                                    Config.GENERAL_SPEC,
+                                                                                    "seasonhud-client.toml");
+    }
+
+    else {
+      fuzs.forgeconfigapiport.forge.api.neoforge.v4.NeoForgeConfigRegistry.INSTANCE.register(Common.MOD_ID,
+                                               ModConfig.Type.CLIENT,
+                                               Config.GENERAL_SPEC,
+                                               "seasonhud-client.toml");
+    }
+
 
     modEventBus.addListener(SeasonHudForge::onInitialize);
     modEventBus.addListener(SeasonHudForge::ftbChunkSetup);
