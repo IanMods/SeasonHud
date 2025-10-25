97bca4849f3bf891fe29cbe229f596ee9c71ad33
Ian Anderson <lanAnderson@users.noreply.github.com>
2025-10-25 08:37:11 -0700

    Fixed incorrect day count when season lengths are changed in Fabric Seasons.

--- common/src/main/java/club/iananderson/seasonhud/impl/seasons/mods/FabricSeasonsHelper.java
+++ common/src/main/java/club/iananderson/seasonhud/impl/seasons/mods/FabricSeasonsHelper.java
@@ -65,7 +65,7 @@ public class FabricSeasonsHelper implements ISeasonModHelper {
   @Override
   public long getDate(Player player) {
     long dayLength = SeasonHudServer.getDayLength();
-    long seasonLength = FabricSeasons.CONFIG.getSpringLength();
+    int seasonLength = FabricSeasons.getCurrentSeason(player.level()).getSeasonLength();
     long timeToNextSeason = FabricSeasons.getTimeToNextSeason(player.level());
 
     // Get the current day of month from the system. Used with fabric seasons' system time tied with season option
@@ -80,7 +80,8 @@ public class FabricSeasonsHelper implements ISeasonModHelper {
   @Override
   public int seasonDuration(Player player) {
     int dayLength = SeasonHudServer.getDayLength();
+    int seasonLength = FabricSeasons.getCurrentSeason(player.level()).getSeasonLength();
 
-    return FabricSeasons.CONFIG.getSpringLength() / dayLength;
+    return seasonLength / dayLength;
   }
 }
