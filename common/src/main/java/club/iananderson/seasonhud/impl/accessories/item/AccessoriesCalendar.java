560d3cd4a851252481acda91b9571bbca677fb34
Ian Anderson <lanAnderson@users.noreply.github.com>
2025-12-20 11:05:54 -0700

    1.21.10 update

--- common/src/main/java/club/iananderson/seasonhud/impl/accessories/item/AccessoriesCalendar.java
+++ common/src/main/java/club/iananderson/seasonhud/impl/accessories/item/AccessoriesCalendar.java
@@ -4,17 +4,16 @@ import club.iananderson.seasonhud.Common;
 import club.iananderson.seasonhud.impl.seasons.CommonSeasonHelper;
 import com.mojang.blaze3d.vertex.PoseStack;
 import io.wispforest.accessories.api.client.AccessoriesRendererRegistry;
+import io.wispforest.accessories.api.client.AccessoryRenderState;
 import io.wispforest.accessories.api.client.renderers.AccessoryRenderer;
 import io.wispforest.accessories.api.client.renderers.SimpleAccessoryRenderer;
 import io.wispforest.accessories.api.core.Accessory;
 import io.wispforest.accessories.api.core.AccessoryRegistry;
-import io.wispforest.accessories.api.slot.SlotPath;
 import net.minecraft.client.model.EntityModel;
 import net.minecraft.client.model.HumanoidModel;
 import net.minecraft.client.renderer.entity.state.HumanoidRenderState;
 import net.minecraft.client.renderer.entity.state.LivingEntityRenderState;
 import net.minecraft.world.item.Item;
-import net.minecraft.world.item.ItemStack;
 
 public class AccessoriesCalendar implements Accessory {
   public AccessoriesCalendar() {
@@ -33,8 +32,9 @@ public class AccessoriesCalendar implements Accessory {
   public static class Renderer implements SimpleAccessoryRenderer {
 
     @Override
-    public <S extends LivingEntityRenderState> void align(ItemStack stack, SlotPath path, EntityModel<S> model,
-        S renderState, PoseStack matrices) {
+    public <S extends LivingEntityRenderState> void align(AccessoryRenderState accessoryState, S entityState,
+        EntityModel<S> model, PoseStack matrices) {
+
       if (!(model instanceof HumanoidModel<? extends HumanoidRenderState> humanoidModel)) {
         return;
       }
@@ -42,6 +42,7 @@ public class AccessoriesCalendar implements Accessory {
       matrices.scale(0.4F, 0.4F, 0.4F);
       AccessoryRenderer.transformToModelPart(matrices, humanoidModel.body, 0.75, -1, null);
       matrices.translate(-0.25F, -1.75F, -0.72F);
+
     }
   }
 }
