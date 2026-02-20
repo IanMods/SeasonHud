b3c3916951e9c71f9c890433660d20b7a0365ea6
Ian Anderson <lanAnderson@users.noreply.github.com>
2026-02-20 12:31:42 -0700

    Working Forge

--- forge/src/main/java/club/iananderson/seasonhud/forge/SeasonHudForge.java
+++ forge/src/main/java/club/iananderson/seasonhud/forge/SeasonHudForge.java
@@ -4,14 +4,13 @@ import club.iananderson.seasonhud.Common;
 import club.iananderson.seasonhud.config.SeasonHudClient;
 import club.iananderson.seasonhud.config.SeasonHudServer;
 import club.iananderson.seasonhud.forge.client.SeasonHudForgeClient;
-import club.iananderson.seasonhud.forge.event.ClientEvents;
+import club.iananderson.seasonhud.forge.event.ClientEvents.ClientModBusEvents;
 import club.iananderson.seasonhud.forge.impl.accessory.mods.curios.CuriosCompat;
 import club.iananderson.seasonhud.impl.accessory.mods.accessories.AccessoriesCompat;
 import club.iananderson.seasonhud.impl.minimap.CurrentMinimap;
 import club.iananderson.seasonhud.impl.minimap.mods.ftbchunks.SeasonComponent;
 import fuzs.forgeconfigapiport.forge.api.v5.NeoForgeConfigRegistry;
 import net.minecraftforge.client.event.AddGuiOverlayLayersEvent;
-import net.minecraftforge.client.event.InputEvent.Key;
 import net.minecraftforge.client.event.RegisterKeyMappingsEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.config.ModConfig;
@@ -22,17 +21,19 @@ import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 @Mod(Common.MOD_ID)
 public class SeasonHudForge {
   public SeasonHudForge(FMLJavaModLoadingContext context) {
+    var modBusGroup = context.getModBusGroup();
+
+    FMLCommonSetupEvent.getBus(modBusGroup).addListener(SeasonHudForge::commonSetupEvent);
+    FMLClientSetupEvent.getBus(modBusGroup).addListener(SeasonHudForgeClient::onInitializeClient);
+
     NeoForgeConfigRegistry.INSTANCE.register(Common.MOD_ID, ModConfig.Type.CLIENT, SeasonHudClient.CLIENT_SPEC,
                                              "seasonhud-client.toml");
 
     NeoForgeConfigRegistry.INSTANCE.register(Common.MOD_ID, ModConfig.Type.SERVER, SeasonHudServer.SERVER_SPEC,
                                              "seasonhud-server.toml");
 
-    FMLCommonSetupEvent.getBus(context.getModBusGroup()).addListener(SeasonHudForge::commonSetupEvent);
-    FMLClientSetupEvent.getBus(context.getModBusGroup()).addListener(SeasonHudForgeClient::onInitializeClient);
-    Key.BUS.addListener(ClientEvents::onKeyInput);
-    RegisterKeyMappingsEvent.getBus(context.getModBusGroup()).addListener(ClientEvents::onKeyRegister);
-    AddGuiOverlayLayersEvent.getBus(context.getModBusGroup()).addListener(ClientEvents::registerGuiOverlays);
+    RegisterKeyMappingsEvent.getBus(context.getModBusGroup()).addListener(ClientModBusEvents::onKeyRegister);
+    AddGuiOverlayLayersEvent.getBus(context.getModBusGroup()).addListener(ClientModBusEvents::registerGuiOverlays);
   }
 
   public static void commonSetupEvent(FMLCommonSetupEvent event) {
@@ -50,7 +51,7 @@ public class SeasonHudForge {
     });
   }
 
-  public static void ftbChunkSetup(FMLCommonSetupEvent event) {
+  private static void ftbChunkSetup(FMLCommonSetupEvent event) {
     if (CurrentMinimap.ftbChunksLoaded()) {
       event.enqueueWork(SeasonComponent::ftbChunkSetup);
     }
