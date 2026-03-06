74d5d4831c13b0ada5d1e645927edbfe26e6911c
Ian Anderson <lanAnderson@users.noreply.github.com>
2026-03-06 09:45:22 -0700

    1.21.1 changes

--- common/src/main/java/club/iananderson/seasonhud/client/gui/screens/MainOptionsScreen.java
+++ common/src/main/java/club/iananderson/seasonhud/client/gui/screens/MainOptionsScreen.java
@@ -4,14 +4,12 @@ import club.iananderson.seasonhud.Common;
 import club.iananderson.seasonhud.client.gui.components.buttons.MenuButton;
 import club.iananderson.seasonhud.client.gui.components.buttons.MenuButton.MenuButtons;
 import club.iananderson.seasonhud.config.SeasonHudClient;
-import club.iananderson.seasonhud.impl.minimap.mods.journeymap.JourneymapSeasonPlugin;
 import club.iananderson.seasonhud.platform.Services;
 import java.util.Arrays;
 import journeymap.client.ui.UIManager;
 import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.client.gui.components.Button;
 import net.minecraft.client.gui.components.CycleButton;
-import net.minecraft.client.gui.components.Tooltip;
 import net.minecraft.network.chat.Component;
 import org.jspecify.annotations.NonNull;
 
@@ -120,9 +118,8 @@ public class MainOptionsScreen extends SeasonHudScreen {
       row += 2;
       journeyMapRow = row;
 
-      journeyMapButton = MenuButton.builder(MenuButtons.JOURNEYMAP, (button) -> JourneymapSeasonPlugin.getInstance()
-              .getClientProperties()
-              .openAddonOptionsEditor(this))
+      journeyMapButton = MenuButton.builder(MenuButtons.JOURNEYMAP,
+                                            (button) -> UIManager.INSTANCE.openAddonOptionsEditor(this, true))
           .withTooltip(Common.newTooltip("menu.seasonhud.main.journeymap.options.tooltip"))
           .withPos(leftButtonX, (buttonStartY + (row * offsetY))).withWidth(buttonWidth)
           .build();
--- forge/src/main/java/club/iananderson/seasonhud/forge/event/ClientEvents.java
+++ forge/src/main/java/club/iananderson/seasonhud/forge/event/ClientEvents.java
@@ -3,7 +3,6 @@ package club.iananderson.seasonhud.forge.event;
 import club.iananderson.seasonhud.Common;
 import club.iananderson.seasonhud.client.KeyBindings;
 import club.iananderson.seasonhud.client.SeasonHudClientCommon;
-import club.iananderson.seasonhud.forge.client.overlays.JourneyMap;
 import club.iananderson.seasonhud.forge.client.overlays.SeasonHudOverlay;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.client.event.AddGuiOverlayLayersEvent;
