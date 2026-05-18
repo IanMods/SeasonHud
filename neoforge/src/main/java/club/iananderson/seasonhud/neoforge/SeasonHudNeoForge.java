278e5da23e3a0f827df07a02f0b7b0d8692c37cd
Ian Anderson <lanAnderson@users.noreply.github.com>
2026-05-17 21:56:20 -0700

    ### Fixed
    - Fixed Neoforge server crash

--- neoforge/src/main/java/club/iananderson/seasonhud/neoforge/SeasonHudNeoForge.java
+++ neoforge/src/main/java/club/iananderson/seasonhud/neoforge/SeasonHudNeoForge.java
@@ -15,8 +15,6 @@ import net.neoforged.fml.event.lifecycle.FMLCommonSetupEvent;
 public class SeasonHudNeoForge {
 
   public SeasonHudNeoForge(IEventBus modEventBus, ModContainer modContainer) {
-    Common.init();
-
     modContainer.registerConfig(ModConfig.Type.CLIENT, SeasonHudClient.CLIENT_SPEC, "seasonhud-client.toml");
     modContainer.registerConfig(ModConfig.Type.SERVER, SeasonHudServer.SERVER_SPEC, "seasonhud-server.toml");
 
